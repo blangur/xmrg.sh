96a26ccd704219b08b139225286c0decda00d489
blangur <37187145+blangur@users.noreply.github.com>
2021-05-18 12:18:43 +0700

    Create xmr.sh

---
+++ xmr.sh
@@ -0,0 +1,5 @@
+# xmr
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
+tar -zxf xmrig-6.12.1-linux-x64.tar.gz
+./xmrig --donate-level 1 -o pool.hashvault.pro:443 -u 86uizZH4H5nMVK1CC1bk9B4avu7RSXSRpdDeKkFEp6APQrpNBvdMyFpHbXcqNdXHKtYkSSmaRPWDWdhqypdmcUb3LdSTv6N -p Tambngimb -k -t2 --tls
